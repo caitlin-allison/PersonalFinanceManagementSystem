fe668ab76a355d74fbb45a31a53558db43d72cb6
Fatima Marquez <fatimamarquez0914@gmail.com>
2025-04-12 16:59:14 -0500

    Updated navigation with Screens

---
+++ MyFinanceApp/screens/ExpensesScreen.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { View, Text, Button, StyleSheet, TouchableOpacity } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+
+export default function ExpensesScreen({ navigation }) {
+  React.useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <TouchableOpacity onPress={() => navigation.openDrawer()} style={{ marginRight: 15 }}>
+          <Ionicons name="menu" size={30} />
+        </TouchableOpacity>
+      ),
+    });
+  }, [navigation]);
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Expenses Screen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: { flex: 1, justifyContent: 'center', alignItems: 'center' },
+  title: { fontSize: 24, fontWeight: 'bold' },
+});
